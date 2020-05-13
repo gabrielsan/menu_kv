574e666df3a55a34e5369d63331e08ae1699ea38
Gabriel dos Santos Sanches <gabrielssanches@gmail.com>
2020-05-13 11:05:01 -0400

    Renames init_val to std_val

--- src/kv.c
+++ src/kv.c
@@ -6,25 +6,25 @@
 #define KV_META_ATTR aligned
 #endif /* KV_META_ATTR */
 
-#define KV_I32_META_OP(keyname_, init_val_, min_val_, max_val_, options_) \
+#define KV_I32_META_OP(keyname_, std_val_, min_val_, max_val_, options_)            \
 {                                                                                   \
-    .init_val = (uint32_t)init_val_,                                                \
+    .std_val = (uint32_t)std_val_,                                                  \
     .max_val = (uint32_t)max_val_,                                                  \
     .min_val = (uint32_t)min_val_,                                                  \
     .op = options_,                                                                 \
     .ptr = NULL,                                                                    \
 },
 
-#define KV_ENUM_OP_META(keyname_, init_val_, max_val_, ...) \
+#define KV_ENUM_OP_META(keyname_, std_val_, max_val_, ...)  \
 {                                                           \
-    .init_val = (uint32_t)init_val_,                        \
+    .std_val = (uint32_t)std_val_,                          \
     .max_val = (uint32_t)(max_val_ - 1),                    \
     .min_val = 0,                                           \
     .op = 0,                                                \
     .ptr = kv_enum_strlist_ ## keyname_,                    \
 },
 
-#define KV_ENUM_OP_STR_LIST(keyname_, init_val_, max_val_, ...)                            \
+#define KV_ENUM_OP_STR_LIST(keyname_, std_val_, max_val_, ...)                            \
     static const char *kv_enum_strlist_ ## keyname_[] = { __VA_ARGS__ __VA_OPT__(,) NULL}; \
 
 __attribute__ ((KV_META_ATTR))
@@ -38,12 +38,16 @@ static const kv_meta_t kv_enum_meta[KV_ENUM_NITEMS] = {KV_ENUM_LIST(KV_ENUM_OP_M
 
 static int32_t kv_i32_value[KV_I32_NITEMS];
 
+void kv_init(void) {
+
+}
+
 void kv_print(void) {
 
     for(int k = KV_I32_LIST_START; k < KV_I32_NITEMS; k++) {
         printf("#%d = %d\n", k, kv_i32_value[k]);
         
-        printf("#%d [%d|%d|%d|%d]\n", k, kv_i32_meta[k].init_val, kv_i32_meta[k].max_val, kv_i32_meta[k].min_val, kv_i32_meta[k].op);
+        printf("#%d [%d|%d|%d|%d]\n", k, kv_i32_meta[k].std_val, kv_i32_meta[k].max_val, kv_i32_meta[k].min_val, kv_i32_meta[k].op);
     }
     for(int k = 0; k < KV_ENUM_NITEMS; k++) {
         const char **print_str = kv_enum_meta[k].ptr;
--- src/kv.h
+++ src/kv.h
@@ -11,12 +11,12 @@ typedef struct kv_meta_struct {
 	uint32_t op;
 	uint32_t max_val;
 	uint32_t min_val;
-	uint32_t init_val;
+	uint32_t std_val;
 	void *ptr;
 } kv_meta_t;
 
-#define KV_I32_OP_ENUMERATOR(keyname_, init_val_, min_val_, max_val_, options_) keyname_,
-#define KV_ENUM_OP_ENUMERATOR(keyname_, init_val_, max_val_, ...) keyname_,
+#define KV_I32_OP_ENUMERATOR(keyname_, std_val_, min_val_, max_val_, options_) keyname_,
+#define KV_ENUM_OP_ENUMERATOR(keyname_, std_val_, max_val_, ...) keyname_,
 
 #include "kv_conf.h"
 
